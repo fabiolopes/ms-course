ee078cd5d9a61db0bdab15a59a33ae1dd3bbebf0
Fabio Bione <fabiolopesbione@hotmail.com>
2021-05-09 00:54:21 -0300

    Creation config server

---
+++ hr-config-server/src/test/java/com/bios/hreconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bios.hreconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
