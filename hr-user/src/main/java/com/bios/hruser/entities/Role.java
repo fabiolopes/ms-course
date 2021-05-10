f1c932123f0c008d5961fe6270653a246bd33847
Fabio Bione <fabiolopesbione@hotmail.com>
2021-05-09 21:15:28 -0300

    database seeding

--- hr-user/src/main/java/com/bios/hruser/entities/Role.java
+++ hr-user/src/main/java/com/bios/hruser/entities/Role.java
@@ -1,21 +1,15 @@
 package com.bios.hruser.entities;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_user")
+@Table(name = "tb_role")
 public class Role implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -28,13 +22,6 @@ public class Role implements Serializable{
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "tb_user_role",
-		joinColumns = @JoinColumn(name = "user_id"),
-		inverseJoinColumns = @JoinColumn(name = "role_id")
-	)
-	private Set<Role> roles = new HashSet<Role>();
 
 	public Role(Long id, String roleName) {
 		super();
@@ -56,14 +43,6 @@ public class Role implements Serializable{
 
 	public void setRoleName(String roleName) {
 		this.roleName = roleName;
-	}
-
-	public Set<Role> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
 	}
 
 	@Override
